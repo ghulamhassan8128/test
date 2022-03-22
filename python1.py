e032e22b7f8902c6529e398fb3a91726a6f0e3c2
Ghulam Hassan <ghulamhassan@8128>
2022-03-22 02:19:54 -0700

    cricket batting promt

--- python1.py
+++ python1.py
@@ -1,1 +1,5 @@
-print("welcome to python coding")
+x=print(input("Are you Batsman?"))
+if x=="yes":
+    print("You will be played at no 3 ")
+else:
+    print("You will not selected for batting  ")
